d790b03c849d63af6337c924cfaf8e944929dbb1
TechieSwathiK <swathi.codingal@gmail.com>
2025-10-06 18:01:12 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,15 @@
+# Import necessary libraries
+
+
+# Setting up Main Window
+
+
+# Function to open New (Top Level) Window
+
+    # Setting up Top Window
+
+
+# Adding a label and button Widget to Root (Main) Window
+
+
+# Arranging widgets
